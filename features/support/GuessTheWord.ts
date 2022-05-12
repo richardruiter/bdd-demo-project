7a13c304004c2ee50fef75abc3145dc870584014
Richard Ruiter <richard.ruiter@gmail.com>
2022-05-12 21:57:55 +0200

    initial commit

---
+++ features/support/GuessTheWord.ts
@@ -0,0 +1,27 @@
+import { Given, Then, When } from "@cucumber/cucumber";
+
+Given("the Maker has started a game with the word {string}", function (string) {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+When("the Breaker joins the Maker's game", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+When("the Maker starts a game", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Then("the Breaker must guess a word with {int} characters", function (int) {
+  // Then('the Breaker must guess a word with {float} characters', function (float) {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Then("the Maker waits for a Breaker to join", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
